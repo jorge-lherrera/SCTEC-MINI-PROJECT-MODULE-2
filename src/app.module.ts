98131f99eda735f69414fa82e07dd829feb02cf9
Jorge Herrera <jorgeluishm88@gmail.com>
2026-09-13 10:57:41 -0300

    feat: implement user repository layer and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './database/data-source';
+import { UserModule } from './modules/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
   ],
   controllers: [],
   providers: [],
