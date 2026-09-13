6271192b013d3e73defc860a3d3ec11a3ebedaef
Jorge Herrera <jorgeluishm88@gmail.com>
2026-09-13 11:21:06 -0300

    feat: implement login with JWT issuance

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
 import { AuthController } from '../controllers/auth.controller';
+import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
 import { UserModule } from './user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<JwtSignOptions['expiresIn']>(
+            'JWT_EXPIRES_IN',
+          ),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [UserService],
+  providers: [AuthService, UserService],
+  exports: [JwtModule],
 })
 export class AuthModule {}
